5b17407d73923948d69e136912d029c77e40a8d4
Unknown <ngoalongtb001@gmail.com>
2018-12-14 21:05:52 +0700

    thread pool update

--- AudioMaker/audio_maker.py
+++ AudioMaker/audio_maker.py
@@ -18,11 +18,64 @@ try:
     end = int(args[4])
 except:
 	pass
+import sys
+IS_PY2 = sys.version_info < (3, 0)
+
+if IS_PY2:
+    from Queue import Queue
+else:
+    from queue import Queue
+
+from threading import Thread
+
+
+class Worker(Thread):
+    """ Thread executing tasks from a given tasks queue """
+    def __init__(self, tasks):
+        Thread.__init__(self)
+        self.tasks = tasks
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        while True:
+            func, args, kargs = self.tasks.get()
+            try:
+                func(*args, **kargs)
+            except Exception as e:
+                # An exception happened in this thread
+                print(e)
+            finally:
+                # Mark this task as done, whether an exception happened or not
+                self.tasks.task_done()
+
+
+class ThreadPool:
+    """ Pool of threads consuming tasks from a queue """
+    def __init__(self, num_threads):
+        self.tasks = Queue(num_threads)
+        for _ in range(num_threads):
+            Worker(self.tasks)
+
+    def add_task(self, func, *args, **kargs):
+        """ Add a task to the queue """
+        self.tasks.put((func, args, kargs))
+
+    def map(self, func, args_list):
+        """ Add a list of tasks to the queue """
+        for args in args_list:
+            self.add_task(func, args)
+
+    def wait_completion(self):
+        """ Wait for completion of all the tasks in the queue """
+        self.tasks.join()
+
+    
+
 
 def run_all(i):
-    subprocess.run(f"python app.py run_all ../data/{name}/chuong-{i} {voice} -1 0")
+    subprocess.run(f"python app.py run_all ../data/{name}/chuong-{i} {voice} -2 0")
 
-for i in range(start, end + 1):
-    threading.Thread(target=run_all, args=([i])).start()
-	# t2 = threading.Thread(target=cal_cube, args=(arr,))
-    # subprocess.run(f"python app.py run_all ../data/{name}/chuong-{i} {voice} -1 0")
+pool = ThreadPool(5)
+pool.map(run_all, range(start, end + 1))
+pool.wait_completion()
